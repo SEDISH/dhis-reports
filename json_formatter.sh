8e9e1ad18061e551f3a3a83d4bd3ebe33b2cbf85
Arkadiusz Lalo <alalo@soldevelo.com>
2018-06-18 18:12:42 +0200

    Removed the last comma from the result list

--- json_formatter.sh
+++ json_formatter.sh
@@ -1,7 +1,7 @@
 echo "Executing $1 script"
 USER=root
 DB=isanteplus
-mysql -u $USER -p $DB < $1 | sed 's/}/},/g' | sed 1d > $1-results.json
+mysql -u $USER -p $DB < $1 | sed 's/}/},/g' | sed 1d | sed '$ s/.$//' > $1-results.json
 echo "Results saved to: $1-results.json"
 
 
