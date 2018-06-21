2eeb024debbca0d44cb885fed7c080c46fb13f71
Wojciech Buława <wbulawa@soldevelo.com>
2018-06-21 15:00:44 +0200

    Added Tracked Entities' post body

---
+++ patient_status_tracked_entity.sql
@@ -0,0 +1,48 @@
+USE isanteplus;
+SET @default_group_concat_max_len = 1024;
+SET @max_group_concat_max_len = 4294967295;
+
+SET SESSION group_concat_max_len = @max_group_concat_max_len;
+
+SELECT (SELECT CONCAT( "{\"trackedEntityInstances\": ", instances.entity_instance, "}")
+FROM (SELECT CONCAT('[', instance.array, ']') as entity_instance
+  FROM (SELECT GROUP_CONCAT(entities_list.track_entity SEPARATOR ',') AS array
+    FROM (
+      SELECT JSON_OBJECT (
+        "trackedEntity", "MCPQUTHX1Ze",
+        "orgUnit", "Vih6emBLLmw",
+        "attributes", JSON_ARRAY(
+          JSON_OBJECT(
+            "attribute", "py0TvSTBlrr",
+            "value", pat.given_name
+            ),
+          JSON_OBJECT(
+            "attribute", "uWUIkGpSMa6",
+            "value", pat.family_name
+            ),
+          JSON_OBJECT(
+            "attribute", "Cn9LcaW7Orr",
+            "value", pat.identifier
+            )
+          ),
+        "enrollments", JSON_ARRAY(
+          JSON_OBJECT(
+            "orgUnit", "Vih6emBLLmw",
+            "program", "x2NBbIpHohD",
+            "enrollmentDate", DATE_FORMAT(DATE(NOW()), "%Y%m%d"),
+            "incidentDate", DATE_FORMAT(MAX(patstatus.start_date), "%Y%m%d")
+            )
+          )
+        ) AS track_entity
+      FROM isanteplus.patient pat
+      INNER JOIN isanteplus.patient_status_arv patstatus
+      ON pat.patient_id=patstatus.patient_id
+      INNER JOIN isanteplus.arv_status_loockup arv
+      ON patstatus.id_status=arv.id
+      GROUP BY pat.patient_id
+    ) AS entities_list
+  ) AS instance
+) AS instances)
+INTO OUTFILE '/var/lib/mysql-files/patient_status_tracked_entity.json';
+
+SET SESSION group_concat_max_len = @default_group_concat_max_len;
