91a8c2f6d90950f926ada830aaea24baaf4abeff
Arkadiusz Lalo <alalo@soldevelo.com>
2018-06-22 17:00:24 +0200

    Corrected the copy script

--- cp_jsons.sh
+++ cp_jsons.sh
@@ -3,7 +3,7 @@ if [ -z "$1" ]
 then
   echo 'Pass destination catalog as an argument'
 else
-  sudo find /var/lib/mysql-files/ | egrep 'patient_status.*\.json' | sudo xargs cp -t  $1
-  sudo find /var/lib/mysql-files/ | egrep 'patient_status.*\.json' | sudo xargs rm
-  sudo find $1 | egrep 'patient_status.*\.json' | sudo xargs  chmod -v 777
+  sudo find /var/lib/mysql-files/ | egrep '.*\.json' | sudo xargs cp -t  $1
+  sudo find /var/lib/mysql-files/ | egrep '.*\.json' | sudo xargs rm
+  sudo find $1 | egrep '.*\.json' | sudo xargs  chmod -v 777 1> /dev/null
 fi
