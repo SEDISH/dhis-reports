a8ab950cd1fa0e3b495e450f313eab2e5438726a
Paweł Gesek <pgesek@soldevelo.com>
2018-06-25 16:51:03 +0200

    Fix exposed infant PCR report

--- report_scripts/number_exposed_infants_tested_by_pcr.sql
+++ report_scripts/number_exposed_infants_tested_by_pcr.sql
@@ -4,7 +4,7 @@ select json_object(
 "value", Total,
 "period", DATE_FORMAT(Dt, "%Y%m%d")) as results
 from(
-SELECT COUNT(DISTINCT phv.patient_id) as Total, vt.encounter_date as Dt
+SELECT 1 as Total, phv.patient_id, vt.encounter_date as Dt
 FROM isanteplus.pediatric_hiv_visit phv
 LEFT OUTER JOIN  isanteplus.patient_laboratory plab
 ON plab.patient_id=phv.patient_id
@@ -16,5 +16,6 @@ WHERE(
 		OR(plab.test_id=844 AND plab.test_result IN(1301,1302,1300,1304) AND plab.date_test_done IS NOT NULL)
 	)
 AND phv.actual_vih_status=1405
-GROUP BY vt.encounter_date
+GROUP BY vt.encounter_date, phv.patient_id
 ) A
+
