a99183d22f96745efa47427764da109f80df4959
Arkadiusz Lalo <alalo@soldevelo.com>
2018-07-04 17:04:56 +0200

    Added clearing of the analytics tables cache

--- main.sh
+++ main.sh
@@ -12,6 +12,10 @@ DHIS_PASSWORD=$3;
 python3 etl_extension/org_units/sync_org_unit.py $DB_PASS $DHIS_PASSWORD $DHIS_URL
 ./generateAllData.sh $DB_PASS;
 ./deleteAllProgramsData.sh $DHIS_URL $DHIS_PASSWORD;
+
+curl -k -X POST -u "$USERNAME:$DHIS_PASSWORD" "http://$DHIS_URL/api/26/maintenance/analyticsTablesClear" 2>&1 | \
+  awk -v date="$(date +"%Y-%m-%d %r")" '{print date ": " $0}' >> logs/analyticsRun.log;
+
 ./sendAllData.sh $DHIS_URL $DHIS_PASSWORD;
 
 curl -k -X POST -u "$USERNAME:$DHIS_PASSWORD" "http://$DHIS_URL/api/25/resourceTables/analytics" 2>&1 | \
