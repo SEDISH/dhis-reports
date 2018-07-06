154d37b4982aedc1ccb38c082fade7e6ab016480
Arkadiusz Lalo <alalo@soldevelo.com>
2018-07-06 16:39:09 +0200

    Chenged absolute paths to relative

--- deleteAllProgramsData.sh
+++ deleteAllProgramsData.sh
@@ -6,16 +6,17 @@ fi
 
 DHIS_URL=$1
 DHIS_PASSWORD=$2
+scriptDir=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
 
-./deleteProgramEvents.sh $DHIS_URL $DHIS_PASSWORD mfyC6GCw1IH # hiv_patient_with_activity_after_disc
-./deleteProgramEvents.sh $DHIS_URL $DHIS_PASSWORD mBMK6RRLKEZ # list_pregnancy_women_receiving_in_clinic
-./deleteProgramEvents.sh $DHIS_URL $DHIS_PASSWORD x2NBbIpHohD # patient_status
-./deleteProgramEvents.sh $DHIS_URL $DHIS_PASSWORD JnV2CR1UKIZ # patientArvEnd
-./deleteProgramEvents.sh $DHIS_URL $DHIS_PASSWORD cBI32y2KeC9 # patientNextArvInThirtyDay
-./deleteProgramEvents.sh $DHIS_URL $DHIS_PASSWORD ewmeREcqCmN # patientStartingArv
-./deleteProgramEvents.sh $DHIS_URL $DHIS_PASSWORD rCJQM1bvXYm # visitNextFourteenDays
-./deleteProgramEvents.sh $DHIS_URL $DHIS_PASSWORD Lh9TkmcZf4a # patient_with_only_register_form
-./deleteProgramEvents.sh $DHIS_URL $DHIS_PASSWORD iTlI6sz0KWM # visitNextSevenDays
-# ./deleteProgramEvents.sh $DHIS_URL $DHIS_PASSWORD Rnvvg6utP5O # consultationByDay
-./deleteProgramEvents.sh $DHIS_URL $DHIS_PASSWORD lV4LM75LrPt # hivPatientWithoutFirstVisit
-./deleteProgramEvents.sh $DHIS_URL $DHIS_PASSWORD Q7pD6QSyVwF # dashboard_event
+$scriptDir/deleteProgramEvents.sh $DHIS_URL $DHIS_PASSWORD mfyC6GCw1IH # hiv_patient_with_activity_after_disc
+$scriptDir/deleteProgramEvents.sh $DHIS_URL $DHIS_PASSWORD mBMK6RRLKEZ # list_pregnancy_women_receiving_in_clinic
+$scriptDir/deleteProgramEvents.sh $DHIS_URL $DHIS_PASSWORD x2NBbIpHohD # patient_status
+$scriptDir/deleteProgramEvents.sh $DHIS_URL $DHIS_PASSWORD JnV2CR1UKIZ # patientArvEnd
+$scriptDir/deleteProgramEvents.sh $DHIS_URL $DHIS_PASSWORD cBI32y2KeC9 # patientNextArvInThirtyDay
+$scriptDir/deleteProgramEvents.sh $DHIS_URL $DHIS_PASSWORD ewmeREcqCmN # patientStartingArv
+$scriptDir/deleteProgramEvents.sh $DHIS_URL $DHIS_PASSWORD rCJQM1bvXYm # visitNextFourteenDays
+$scriptDir/deleteProgramEvents.sh $DHIS_URL $DHIS_PASSWORD Lh9TkmcZf4a # patient_with_only_register_form
+$scriptDir/deleteProgramEvents.sh $DHIS_URL $DHIS_PASSWORD iTlI6sz0KWM # visitNextSevenDays
+# $scriptDir/deleteProgramEvents.sh $DHIS_URL $DHIS_PASSWORD Rnvvg6utP5O # consultationByDay
+$scriptDir/deleteProgramEvents.sh $DHIS_URL $DHIS_PASSWORD lV4LM75LrPt # hivPatientWithoutFirstVisit
+$scriptDir/deleteProgramEvents.sh $DHIS_URL $DHIS_PASSWORD Q7pD6QSyVwF # dashboard_event
